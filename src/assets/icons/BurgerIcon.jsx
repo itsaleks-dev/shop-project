de37d9b4eeaf6675c371d67d81d941271cf7e5eb
Aleks <develop@MacBook-Air-Aleks.local>
2025-09-20 02:53:58 +0300

    added BurgerIcon.jsx

---
+++ src/assets/icons/BurgerIcon.jsx
@@ -0,0 +1,9 @@
+const BurgerIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+        <rect x="3" y="6" width="18" height="2" rx="1" fill="#292D32" />
+        <rect x="3" y="12" width="18" height="2" rx="1" fill="#292D32" />
+        <rect x="3" y="18" width="18" height="2" rx="1" fill="#292D32" />
+    </svg>
+);
+
+export default BurgerIcon;
--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -1,1 +1,7 @@
+import React from 'react';
+import Burger from "./Burger";
+import Menu from "./Menu";
+import Logo from "./Logo";
+import Userbar from "./Userbar";
+
 import './Header.scss';
